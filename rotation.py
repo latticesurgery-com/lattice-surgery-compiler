507b6e07c9741994f40c84d2b2b63099801834eb
Alex Nguyen <hmn5@sfu.ca>
2020-10-30 02:07:26 -0700

    add Measurement class

--- rotation.py
+++ rotation.py
@@ -89,3 +89,60 @@ class Rotation(object):
             Gate: Gate of targeted qubit.
         """
         return self.gate_list[qubit]
+
+    
+
+class Measurement(object):
+    """
+    Representing a Pauli Product Measurement Block
+
+    """
+    def __init__(self, no_of_qubit: int, isNegative: bool = False) -> None:
+        """
+        Generate a Pauli Product Measurement Block. All gates inside are initialized to Identity.
+        A Pauli Product Measurement Block MUST span all qubits in the circuit
+
+        Args:
+            no_of_qubit (int): Number of qubits in the circuit
+            isNegative (bool, optional): Set to negative. Defaults to False.
+        """
+        self.qubit_num = no_of_qubit
+        self.isNegative = isNegative
+        self.gate_list = [Gate(0, i) for i in range(no_of_qubit)]
+        
+    def __str__(self):
+        return '{}M: {}'.format('-' if self.isNegative else '', self.gate_list) 
+        
+    
+    def __repr__(self):
+        return str(self)    
+    
+    
+    def copy(self):
+        new_rotation = Rotation(self.qubit_num, self.isNegative)
+        self.gate_list = [g.copy() for g in self.gate_list]
+        return new_rotation
+
+    
+    def change_gate(self, qubit: int, target_gate: int) -> None:
+        """
+        Modify a Gate in the Pauli Rotation Block
+
+        Args:
+            qubit (int): Targeted qubit
+            target_gate (int): New gate type (0 = I, 1 = X, 2 = Z, 3 = Y)
+        """
+        self.gate_list[qubit] = target_gate
+
+    
+    def return_gate(self, qubit: int) -> Gate:
+        """
+        Return the current gate of qubit i. 
+
+        Args:
+            qubit (int): Targeted qubit
+
+        Returns:
+            Gate: Gate of targeted qubit.
+        """
+        return self.gate_list[qubit]
