2b7449070c3bd3054d8333aba3f7a7b5b23371e8
George Watkins <gwwatkin@sfu.ca>
2021-02-13 00:45:54 -0800

    Update deploy script to only execute when changes are made

--- webgui/call_deploy.sh
+++ webgui/call_deploy.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-./deploy.sh 2>&1 | tee -a deploy.log
+
+DEPLOY_BRANCH=webgui-deploy
+
+git fetch origin webgui-deploy
+new_commits=$(git log HEAD..origin/$DEPLOY_BRANCH --oneline)
+if [ -n "$new_commits" ]
+then
+  git reset --hard origin/$DEPLOY_BRANCH
+  ./deploy.sh 2>&1 | tee -a deploy.log
+fi
