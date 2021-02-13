6aeed2e68931a01b27993b2bbb91b2d14efa69ac
George Watkins <gwwatkin@sfu.ca>
2021-02-13 09:29:07 -0800

    Polish and disable depoyment script

--- webgui/deploy.sh
+++ webgui/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo "================ [`date`] running $0 ================ "
-python3.8 -m venv env
-env/bin/ac
-env/bin/activate
-source env/bin/activate
-pip install pyzx  mako pyramid python-igraph
-PYTHONPATH="..:$PYTHONPATH" python serve.py &
+
+#echo "================ [`date`] running $0 ================ "
+#python3.8 -m venv env
+#source env/bin/activate
+#pip install pyzx  mako pyramid python-igraph
+#PYTHONPATH="..:$PYTHONPATH" python serve.py &
