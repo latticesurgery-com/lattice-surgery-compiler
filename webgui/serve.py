94d763a8f61129d9d8e8732ced0c069403e64eeb
George Watkins <gwwatkin@sfu.ca>
2021-02-13 00:00:27 -0800

    Enable lattice view rendering as a dynamic request

--- webgui/serve.py
+++ webgui/serve.py
@@ -1,23 +1,44 @@
+import circuit
+from pauli_rotations_to_lattice_surgery import pauli_rotation_to_lattice_surgery_computation
+import webgui.lattice_view
+
 from wsgiref.simple_server import make_server
+import mako.template
 from pyramid.config import Configurator
 from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.request import Request
 
+import os
+import uuid
+import shutil
 
-# Lattice view imports
-import webgui.lattice_view
-import mako.template as mako
 
 
 @view_config(route_name='upload_circuit')
 def upload_circuit(request):
-    page = mako.Template(filename="templates/upload_circuit.mak")
+    page = mako.template.Template(filename="templates/upload_circuit.mak")
     return Response(page.render(request=request))
 
 
 @view_config(route_name='lattice_view')
-def view_compiled(request):
-    return Response('View Compiled')
+def view_compiled(request : Request):
+
+    # Save the file to a tmp dir so that pyzx can read it
+    file_ext = request.POST['circuit'].filename.split('.')[-1]
+    circuit_tmp_save_location = os.path.join('/tmp','%s.%s'%(uuid.uuid4(),file_ext))
+    intput_circuit_file = request.POST['circuit'].file
+    intput_circuit_file.seek(0)
+    with open(circuit_tmp_save_location, 'wb') as output_file:
+        shutil.copyfileobj(intput_circuit_file, output_file)
+
+    input_circuit = circuit.Circuit.load_from_file(circuit_tmp_save_location)
+    print(input_circuit.render_ascii())
+    os.unlink(circuit_tmp_save_location)
+
+    # TODO refactor the process of getting slices into circuit
+    lsc = pauli_rotation_to_lattice_surgery_computation(input_circuit)
+    return Response(webgui.lattice_view.render_html(lsc.composer.getSlices()))
 
 
 if __name__ == '__main__':
