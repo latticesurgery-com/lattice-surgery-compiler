de8ac348fd9ebf6e6361f023ca1f7712514e8069
George Watkins <gwwatkin@sfu.ca>
2021-03-15 23:03:12 -0700

    Add debug files for simulation and state separation

--- debug/debug_simulation_separations.py
+++ debug/debug_simulation_separations.py
@@ -1,4 +1,5 @@
 from lattice_surgery_computation_composer import *
+from logical_patch_state_simulation import qk
 from webgui import lattice_view
 
 if __name__ == "__main__":
@@ -17,5 +18,22 @@ if __name__ == "__main__":
     print(c.render_ascii())
 
     logical_comp = LogicalLatticeComputation(c)
+    sim = PatchSimulator(logical_comp)
+
+    def nice_print_dict_of_dict_states(ds):
+        out = "{"
+        for k,v in ds.items():
+            out += "\n\t" + str(k) + " : "
+            out +=        str(v.primitive) + " ,"
+        return out + "}"
+
+
+    for op in logical_comp.ops:
+        print("Bf:",sim.logical_state)
+        print("Op:",op)
+        sim.apply_logical_operation(op)
+        print("Af:", sim.logical_state)
+        print("Seps:",nice_print_dict_of_dict_states(StateSeparator.get_separable_qubits(sim.logical_state)))
+
     lsc = LatticeSurgeryComputation.make_computation_with_simulation(logical_comp, LayoutType.SimplePreDistilledStates)
     lattice_view.render_to_file(lsc.composer.getSlices(), "index.html", template="/home/george/courses/CMPT415_498/code/lattice-surgery-compiler/webgui/templates/lattice_view.mak")
