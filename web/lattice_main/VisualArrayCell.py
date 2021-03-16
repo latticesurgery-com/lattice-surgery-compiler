933b4d17aa6d8f77f622cc1187c6e573108ae6a9
Keelan W <keelan_w@outlook.com>
2021-03-15 17:22:45 -0700

    added missing self arg to VisualArrayCell init input.

--- web/lattice_main/VisualArrayCell.py
+++ web/lattice_main/VisualArrayCell.py
@@ -2,7 +2,7 @@ import patches
 from typing import Dict
 
 class VisualArrayCell:
-    def __init__(patch_type: patches.PatchType, edges: Dict[patches.Orientation, patches.EdgeType]):
+    def __init__(self, patch_type: patches.PatchType, edges: Dict[patches.Orientation, patches.EdgeType]):
         self.edges = edges
         self.patch_type = patch_type
         self.text = None
