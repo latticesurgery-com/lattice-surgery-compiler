6713f5b225af92f6a16e824ecc1454b9d74f3743
George Watkins <gwwatkin@sfu.ca>
2021-03-22 00:59:24 -0700

    Take the compilation pipleine out of the view

--- web/lattice_main/views.py
+++ web/lattice_main/views.py
@@ -3,13 +3,9 @@ from django.shortcuts import render
 import os, sys, uuid, shutil
 from typing import *
 
-import patches, circuit
-from pauli_rotations_to_lattice_surgery import pauli_rotation_to_lattice_surgery_computation
+import lattice_surgery_compilation_pipeline
 
-from .sparse_lattice_to_array import sparse_lattice_to_array
-from qubit_state import *
-
-
+import patches # TODO remove the dependency on this import
 
 def upload_circuit(request):
     context = {}
@@ -20,6 +16,7 @@ def view_compiled(request):
     # Save the file to a tmp dir so that pyzx can read it
     print("post-request",request.FILES['circuit'].name)
 
+    # Handle the uploaded circuit
     file_ext = request.FILES['circuit'].name.split('.')[-1]
     circuit_tmp_save_location = os.path.join('/tmp','%s.%s'%(uuid.uuid4(),file_ext))
     intput_circuit_file = request.FILES['circuit']
@@ -27,20 +24,18 @@ def view_compiled(request):
     with open(circuit_tmp_save_location, 'wb') as output_file:
         shutil.copyfileobj(intput_circuit_file, output_file)
 
-    input_circuit = circuit.Circuit.load_from_file(circuit_tmp_save_location)
-    print(input_circuit.render_ascii())
-    os.unlink(circuit_tmp_save_location)
+    try:
+        slices = lattice_surgery_compilation_pipeline.compile_file(circuit_tmp_save_location)
+    finally:
+        os.unlink(circuit_tmp_save_location)
 
-    # TODO refactor the process of getting slices into circuit
-    lsc = pauli_rotation_to_lattice_surgery_computation(input_circuit)
-    slices = lsc.composer.getSlices()
-    mapped_slices = list(map(sparse_lattice_to_array, slices))
     context = {
-        'slices': mapped_slices,
+        'slices': slices,
         'patches': patches
     }
     return render(request,"lattice_main/lattice_view.html",context)
 
+# TODO restore this for debugging
 # def render_to_file(request,slices, output_file_name): # (slices : List[patches.Lattice], output_file_name : str) -> str
 #     slices = list(map(sparse_lattice_to_array, slices))
 #     styles_map = styles_map
