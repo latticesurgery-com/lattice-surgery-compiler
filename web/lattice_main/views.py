dbc35965f35264b0759a86f3cae0e7d8141b2010
Keelan <keelan_w@outlook.com>
2021-03-23 16:20:49 -0700

    have preset circuit select box correctly picking up any circuits from the assets folder and allowing user to select them for computation. added a go back button on lattice_view. cleaned up some html on lattice_view, like duplicate head,style,body.

--- web/lattice_main/views.py
+++ web/lattice_main/views.py
@@ -22,16 +22,23 @@ def upload_circuit(request):
 
 def view_compiled(request):
     """ url shortcut name: lattice_main-latticeview """
+    
+    # print(request.POST)
 
-    #print("post-request",request.FILES['circuit'].name)
-    file_ext = request.FILES['circuit'].name.split('.')[-1]
-    # Save the file to a tmp dir so that pyzx can read it
+    if "localcircuit" in request.POST: # then select dropdown was used and therefore we have a local copy of the circuit in /assets/
+        filename = request.POST['circuit']
+        file_ext = filename.split('.')[-1]
+        input_circuit_file = open(f"../assets/{filename}", "rb")
+    else:
+        file_ext = request.FILES['circuit'].name.split('.')[-1]
+        # Save the file to a tmp dir so that pyzx can read it
+        input_circuit_file = request.FILES['circuit']
+    
     tmp_abs_path = os.path.abspath("./lattice_main/tmp/")
     circuit_tmp_save_location = os.path.join(tmp_abs_path,'%s.%s'%(uuid.uuid4(),file_ext))
-    intput_circuit_file = request.FILES['circuit']
-    intput_circuit_file.seek(0)
+    input_circuit_file.seek(0)
     with open(circuit_tmp_save_location, 'wb') as output_file:
-        shutil.copyfileobj(intput_circuit_file, output_file)
+        shutil.copyfileobj(input_circuit_file, output_file)
 
     input_circuit = circuit.Circuit.load_from_file(circuit_tmp_save_location)
     print(input_circuit.render_ascii())
@@ -39,9 +46,11 @@ def view_compiled(request):
 
     # TODO refactor the process of getting slices into circuit
     logical_circuit = lscc.LogicalLatticeComputation(input_circuit)
+    if "localcircuit" in request.POST:
+        input_circuit_file.close() # close file
     layout_type = lscc.LayoutType.SimplePreDistilledStates
     lattice_comp = lscc.LatticeSurgeryComputation.make_computation_with_simulation(logical_circuit, layout_type)
-
+    
     slices = lattice_comp.composer.getSlices()
     mapped_slices = list(map(sparse_lattice_to_array, slices))
     context = {
