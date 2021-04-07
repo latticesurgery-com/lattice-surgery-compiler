aea244361c0dcf849e7c27e3e37a67ca2db04768
Alex Nguyen <hoangminh98@gmail.com>
2021-04-07 09:36:13 -0700

    basic test case for dependency tree generator

---
+++ debug/debug_basic_dependency_graphy.py
@@ -0,0 +1,28 @@
+from circuit import *
+from rotation import *
+from dependency_tree import *
+
+I = PauliOperator.I 
+X = PauliOperator.X 
+Z = PauliOperator.Z 
+Y = PauliOperator.Y  
+
+# Example from #71 as the base test case
+c = Circuit(2)
+c.add_pauli_block(Rotation.from_list([X, X],Fraction(1,8)))
+c.add_pauli_block(Rotation.from_list([Z, Z],Fraction(1,4)))
+c.add_pauli_block(Rotation.from_list([X, Z],Fraction(-1,4)))
+c.add_pauli_block(Rotation.from_list([I, X],Fraction(-1,4)))
+c.add_pauli_block(Rotation.from_list([Z, I],Fraction(-1,4)))
+c.add_pauli_block(Rotation.from_list([I, Z],Fraction(-1,4)))
+
+print(c.render_ascii())
+a = DependencyTree.from_circuit(c)
+
+print("Returned terminal nodes:")
+print(a.terminal_node)
+print()
+print("Printing Graph:")
+graph = a.generate_adjacency_list()
+for i in graph:
+    print('{}: {}'.format(i, graph[i]))
