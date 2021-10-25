792271f96cf432c8fc2adf186962fa3c3866fd84
alexnguyenn <hoangminh98@gmail.com>
2021-10-25 16:56:07 -0700

    missing an import

--- lsqecc/simulation/__init__.py
+++ lsqecc/simulation/__init__.py
@@ -4,4 +4,4 @@ from conditional_operation_control import (ConditionalOperation,
 from logical_patch_state_simulation import PatchSimulator
 from qiskit_opflow_utils import StateSeparator
 from qubit_state import (ActiveState, ActivityType, DefaultSymbolicStates,
-                         QubitActivity, QubitState, SymbolicState)
+                         QubitActivity, QubitState, SymbolicState, EntangledState)
