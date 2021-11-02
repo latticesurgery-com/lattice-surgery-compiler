2701b8d852cf5236411fae26610e6356572c2b32
George Watkins <gwwatkin@sfu.ca>
2021-11-02 05:49:23 -0700

    Add enable CORS in local api development server

--- webapi/local_compiler_api_server.py
+++ webapi/local_compiler_api_server.py
@@ -1,40 +1,66 @@
-from webapi import json_pipeline
+import lsqecc.pipeline.json_pipeline as json_pipeline
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import http.client
 import json
 import enum
 import traceback
+import jsonpickle
 from typing import Optional, Dict
 
 
 class Routes(enum.Enum):
-    COMPILE = '/compile'
+    COMPILE = "/compile"
 
 
 class _LocalCompilerApiServer(BaseHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        content_length = int(
+            self.headers["Content-Length"]
+        )  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)  # <--- Gets the data itself
-        print("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n" % (
-            str(self.path), str(self.headers), post_data.decode('utf-8')))
+        print(
+            "POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n"
+            % (str(self.path), str(self.headers), post_data.decode("utf-8"))
+        )
+
         if str(self.path) == Routes.COMPILE.value:
             try:
-                response = json_pipeline.handle(post_data.decode('utf-8'))
+                response = json_pipeline.handle(post_data.decode("utf-8"))
                 self.send_response(response.status)
-                self.send_header('Content-type', 'text/html')
+                self.send_header("Content-type", "text/html")
+                self.send_header("Access-Control-Allow-Origin", "*")
                 self.end_headers()
-                self.wfile.write(response.body.encode('utf-8'))
+                self.wfile.write(jsonpickle.encode(response.body).encode("utf-8"))
             except Exception as e:
                 self.send_response(500)
-                self.send_header('Content-type', 'text/html')
+                self.send_header("Content-type", "text/html")
+                self.send_header("Access-Control-Allow-Origin", "*")
                 self.end_headers()
-                self.wfile.write("{}\n\nRequest was {}".format(traceback.format_exc(), post_data.decode('utf-8')).encode('utf-8'))
+                self.wfile.write(
+                    "{}\n\nRequest was {}".format(
+                        traceback.format_exc(), post_data.decode("utf-8")
+                    ).encode("utf-8")
+                )
         else:
             self.send_response(404)
-            self.send_header('Content-type', 'text/html')
+            self.send_header("Content-type", "text/html")
+            self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
-            self.wfile.write("POST Route not found: {}".format(self.path).encode('utf-8'))
+            self.wfile.write(
+                "POST Route not found: {}".format(self.path).encode("utf-8")
+            )
+
+    # Handle Cors
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header(
+            "Access-Control-Allow-Headers", "X-Requested-With, Content-type"
+        )
+        self.end_headers()
+        self.wfile.write("")
 
 
 SAMPLE_CIRCUIT = """
@@ -52,9 +78,9 @@ x q[0];
 """
 
 JSON_POST_PARAMETERS = {
-    'circuit_source': 'str',  # alternative would be 'file'
-    'circuit': SAMPLE_CIRCUIT,
-    'apply_litinski_transform': True
+    "circuit_source": "str",  # alternative would be 'file'
+    "circuit": SAMPLE_CIRCUIT,
+    "apply_litinski_transform": True,
 }
 
 
@@ -62,16 +88,18 @@ def probe(request_post_params: Optional[Dict] = None):
     if request_post_params is None:
         request_post_params = JSON_POST_PARAMETERS
 
-    conn = http.client.HTTPConnection('localhost:9876')
-    headers = {'Content-type': 'application/json'}
+    conn = http.client.HTTPConnection("localhost:9876")
+    headers = {
+        "Content-type": "application/json",
+    }
 
-    conn.request('POST', '/compile', json.dumps(request_post_params), headers)
+    conn.request("POST", "/compile", json.dumps(request_post_params), headers)
 
     response = conn.getresponse()
     print(response.read().decode())
 
 
 if __name__ == "__main__":
-    server_address = ('', 9876)
+    server_address = ("", 9876)
     httpd = HTTPServer(server_address, _LocalCompilerApiServer)
     httpd.serve_forever()
