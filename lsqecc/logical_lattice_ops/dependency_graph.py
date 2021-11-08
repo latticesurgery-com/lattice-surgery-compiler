b761d454781dd06a6aab2910bb2527adfe797bc9
George Watkins <gwwatkin@sfu.ca>
2021-11-08 13:52:19 -0800

    Bump license to include current year

--- lsqecc/logical_lattice_ops/dependency_graph.py
+++ lsqecc/logical_lattice_ops/dependency_graph.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 - George Watkins and Alex Nguyen
+# Copyright (C) 2020-2021 - George Watkins and Alex Nguyen
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
