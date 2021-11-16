d4114341a73fdd6330fd9c0ef515a4f5ff0d429f
alexnguyenn <hoangminh98@gmail.com>
2021-11-15 18:35:12 -0800

    switch to RTD theme, trying out sphinx_autodoc_typehints

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,12 @@ author = "George Watkins, Alex Nguyen and Contributors"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser", "sphinx.ext.napoleon", "sphinx.ext.autodoc"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -46,7 +51,7 @@ myst_heading_anchors = 3
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
