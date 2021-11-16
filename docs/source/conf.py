36c114db0d64afcf1a3692b84062cf00ee4077d5
alexnguyenn <hoangminh98@gmail.com>
2021-11-16 09:13:23 -0800

    update to furo newest version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ myst_heading_anchors = 3
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
