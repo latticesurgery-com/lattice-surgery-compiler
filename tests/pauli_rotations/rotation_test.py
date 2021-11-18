9299c151b8d9a9cbfc18f82f4716c2c96f6ee0b5
alexnguyenn <hoangminh98@gmail.com>
2021-11-17 21:22:19 -0800

    add tests for `PauliOperator`, seperate test cases into different classes

--- tests/pauli_rotations/rotation_test.py
+++ tests/pauli_rotations/rotation_test.py
@@ -27,61 +27,99 @@ Y = PauliOperator.Y
 Z = PauliOperator.Z
 
 
-@pytest.mark.parametrize(
-    "pauli_rotation_1, pauli_rotation_2",
-    [
-        (PauliRotation(3, Fraction(1, 2)), PauliRotation(3, Fraction(1, 2))),
-        (
-            PauliRotation.from_list([X, Z], Fraction(1, 4)),
-            PauliRotation.from_list([X, Z], Fraction(1, 4)),
-        ),
-        (
-            PauliRotation.from_list([X, Z, Y], Fraction(1, 8)),
-            PauliRotation.from_list([X, Z, Y], Fraction(1, 8)),
-        ),
-    ],
-)
-def test_pauli_rotation_eq(pauli_rotation_1, pauli_rotation_2):
-    assert pauli_rotation_1 == pauli_rotation_2
+class TestPauliRotation:
+    """Tests for PauliRotation"""
+
+    @pytest.mark.parametrize(
+        "pauli_rotation_1, pauli_rotation_2",
+        [
+            (PauliRotation(3, Fraction(1, 2)), PauliRotation(3, Fraction(1, 2))),
+            (
+                PauliRotation.from_list([X, Z], Fraction(1, 4)),
+                PauliRotation.from_list([X, Z], Fraction(1, 4)),
+            ),
+            (
+                PauliRotation.from_list([X, Z, Y], Fraction(1, 8)),
+                PauliRotation.from_list([X, Z, Y], Fraction(1, 8)),
+            ),
+        ],
+    )
+    def test_pauli_rotation_eq(self, pauli_rotation_1, pauli_rotation_2):
+        assert pauli_rotation_1 == pauli_rotation_2
+
+    @pytest.mark.parametrize(
+        "pauli_rotation_1, pauli_rotation_2",
+        [
+            (PauliRotation(3, Fraction(1, 2)), PauliRotation(4, Fraction(1, 2))),
+            (
+                PauliRotation.from_list([X, Z], Fraction(1, 4)),
+                PauliRotation.from_list([X, Y], Fraction(1, 4)),
+            ),
+            (
+                PauliRotation.from_list([X, Z, Y], Fraction(1, 8)),
+                PauliRotation.from_list([Z, X, Y], Fraction(1, 4)),
+            ),
+        ],
+    )
+    def test_pauli_rotation_ne(self, pauli_rotation_1, pauli_rotation_2):
+        assert pauli_rotation_1 != pauli_rotation_2
 
 
-@pytest.mark.parametrize(
-    "pauli_rotation_1, pauli_rotation_2",
-    [
-        (PauliRotation(3, Fraction(1, 2)), PauliRotation(4, Fraction(1, 2))),
-        (
-            PauliRotation.from_list([X, Z], Fraction(1, 4)),
-            PauliRotation.from_list([X, Y], Fraction(1, 4)),
-        ),
-        (
-            PauliRotation.from_list([X, Z, Y], Fraction(1, 8)),
-            PauliRotation.from_list([Z, X, Y], Fraction(1, 4)),
-        ),
-    ],
-)
-def test_pauli_rotation_ne(pauli_rotation_1, pauli_rotation_2):
-    assert pauli_rotation_1 != pauli_rotation_2
+class TestMeasurement:
+    """Tests for Measurement class"""
+
+    @pytest.mark.parametrize(
+        "measurement_1, measurement_2",
+        [
+            (Measurement(3, True), Measurement(3, True)),
+            (Measurement.from_list([X, Z]), Measurement.from_list([X, Z])),
+            (Measurement.from_list([Y, Z, X, I]), Measurement.from_list([Y, Z, X, I])),
+        ],
+    )
+    def test_measurement_eq(measurement_1, measurement_2):
+        assert measurement_1 == measurement_2
+
+    @pytest.mark.parametrize(
+        "measurement_1, measurement_2",
+        [
+            (Measurement(3, True), Measurement(2, True)),
+            (Measurement.from_list([X, Z]), Measurement.from_list([X, Z], True)),
+            (Measurement.from_list([Y, X, I, I]), Measurement.from_list([Y, Z, X, I])),
+        ],
+    )
+    def test_measurement_ne(measurement_1, measurement_2):
+        assert measurement_1 != measurement_2
 
 
-@pytest.mark.parametrize(
-    "measurement_1, measurement_2",
-    [
-        (Measurement(3, True), Measurement(3, True)),
-        (Measurement.from_list([X, Z]), Measurement.from_list([X, Z])),
-        (Measurement.from_list([Y, Z, X, I]), Measurement.from_list([Y, Z, X, I])),
-    ],
-)
-def test_measurement_eq(measurement_1, measurement_2):
-    assert measurement_1 == measurement_2
+class TestPauliOperator:
+    """Tests for PauliOperator"""
 
+    @pytest.mark.parametrize("input", [(I, Z), (X, I), (I, Y), (I, I)])
+    def test_are_commuting(self, input):
+        assert PauliOperator.are_commuting(*input)
 
-@pytest.mark.parametrize(
-    "measurement_1, measurement_2",
-    [
-        (Measurement(3, True), Measurement(2, True)),
-        (Measurement.from_list([X, Z]), Measurement.from_list([X, Z], True)),
-        (Measurement.from_list([Y, X, I, I]), Measurement.from_list([Y, Z, X, I])),
-    ],
-)
-def test_measurement_ne(measurement_1, measurement_2):
-    assert measurement_1 != measurement_2
+    @pytest.mark.parametrize("input", [(X, Y), (Y, X), (Z, X), (X, Z)])
+    def test_are_not_commuting(self, input):
+        assert not PauliOperator.are_commuting(*input)
+
+    def test_are_commuting_raises_type_error(self):
+        with pytest.raises(TypeError):
+            PauliOperator.are_commuting(1, 2)
+
+    @pytest.mark.parametrize("input", [(X, X), (Y, Y), (Z, Z), (I, I)])
+    def test_multiply_operators_same_operators(self, input):
+        assert PauliOperator.multiply_operators(*input) == (1, I)
+
+    def mutliply_operators_raise_type_error(self):
+        with pytest.raises(TypeError):
+            PauliOperator.multiply_operators(1, 2)
+
+    @pytest.mark.parametrize("input", [X, Y, Z])
+    def test_multiply_operators_with_I(self, input):
+        assert PauliOperator.multiply_operators(input, I) == (1, input)
+        assert PauliOperator.multiply_operators(I, input) == (1, input)
+
+    @pytest.mark.parametrize("input, expected", [((Z, X), Y), ((Z, Y), X), ((Y, X), Z)])
+    def test_multiply_operators_non_commuting(self, input, expected):
+        assert PauliOperator.multiply_operators(*input) == (1j, expected)
+        assert PauliOperator.multiply_operators(*reversed(input)) == (-1j, expected)
