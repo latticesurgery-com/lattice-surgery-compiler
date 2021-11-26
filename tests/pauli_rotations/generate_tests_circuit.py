169102a5b78a48e72a460c296cafd3df888c687b
Varun Seshadri <varunseshadri@gmail.com>
2021-11-26 07:57:17 +0100

    Update tests/pauli_rotations/generate_tests_circuit.py
    Co-authored-by: Alex Nguyen <46719079+alexnguyenn@users.noreply.github.com>

--- tests/pauli_rotations/generate_tests_circuit.py
+++ tests/pauli_rotations/generate_tests_circuit.py
@@ -11,7 +11,7 @@ Z = PauliOperator.Z
 
 
 def generate_tests_pauli_op_circuit_equality(
-    case: str,
+    for_eq: bool,
 ) -> List[Tuple[PauliOpCircuit, PauliOpCircuit]]:
     c1 = PauliOpCircuit(4)
     c1.add_pauli_block(PauliRotation.from_list([I, X, I, I], Fraction(1, 8)))
@@ -22,13 +22,10 @@ def generate_tests_pauli_op_circuit_equality(
     c3.add_pauli_block(PauliRotation.from_list([X, Y, Z, I], Fraction(-1, 4)))
     c3.add_pauli_block(PauliRotation.from_list([I, Z, Z, I], Fraction(1, 8)))
     c3.add_pauli_block(Measurement.from_list([Y, X, I, I]))
-    if case == "eq":
+    if for_eq:
         return [(c1, c1), (c2, c2), (c3, c3)]
-    elif case == "ne":
-        return [(c1, c2), (c2, c3), (c3, c1)]
     else:
-        assert False
-
+        return [(c1, c2), (c2, c3), (c3, c1)]
 
 def generate_tests_circuit_has_measurements() -> List[Tuple[PauliOpCircuit, bool]]:
     c1 = PauliOpCircuit(4)
