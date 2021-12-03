692057bde75bf46eb33fd38279c3b18d6355b64d
alexnguyenn <hoangminh98@gmail.com>
2021-12-02 23:16:09 -0800

    Shorten `PauliOpCircuit.__eq__()` implementation

--- src/lsqecc/pauli_rotations/circuit.py
+++ src/lsqecc/pauli_rotations/circuit.py
@@ -50,17 +50,11 @@ class PauliOpCircuit(object):
         return len(self.ops)
 
     def __eq__(self, other) -> bool:
-        if not isinstance(other, PauliOpCircuit):
-            return False
-        elif self.qubit_num != other.qubit_num:
-            return False
-        elif len(self) != len(other):
-            return False
-        else:
-            for op1, op2 in zip(self.ops, other.ops):
-                if op1 != op2:
-                    return False
-            return True
+        return (
+            isinstance(other, PauliOpCircuit)
+            and self.qubit_num == other.qubit_num
+            and self.ops == other.ops
+        )
 
     def __ne__(self, other) -> bool:
         return not self.__eq__(other)
