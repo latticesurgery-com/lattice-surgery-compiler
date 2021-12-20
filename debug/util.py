3c2039d8be2416c2d7984424f368de9f636c677a
George Watkins <36427091+gwwatkin@users.noreply.github.com>
2021-12-19 16:18:22 -0800

    Convert opflow imports from aqua to qiskit core (#191)
    * Convert opflow imports from aqua to qiskit core
    
    * Uniform the calling of qkop

--- debug/util.py
+++ debug/util.py
@@ -1,8 +1,8 @@
 from typing import *
-import qiskit.aqua.operators as qk
+import qiskit.opflow as qkop
 
 
-def nice_print_dict_of_dict_states(ds : Dict[int,qk.DictStateFn]) -> str:
+def nice_print_dict_of_dict_states(ds : Dict[int, qkop.DictStateFn]) -> str:
     out = "{"
     for k ,v in ds.items():
         out += "\n\t" + str(k) + " : "
