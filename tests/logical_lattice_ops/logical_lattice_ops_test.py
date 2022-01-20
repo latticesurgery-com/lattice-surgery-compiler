aaee0542fb75b0d2c77c410eacbd1524fd3ca498
varunseshadri <varunseshadri@gmail.com>
2022-01-20 13:44:49 +0100

    tests for helper class MultiBodyMeasurement

--- tests/logical_lattice_ops/logical_lattice_ops_test.py
+++ tests/logical_lattice_ops/logical_lattice_ops_test.py
@@ -1,9 +1,11 @@
+from typing import Dict
 from unittest.mock import patch
 import pytest
 from lsqecc.pauli_rotations import PauliOpCircuit, PauliRotation, Measurement, PauliOperator
 
 from lsqecc.logical_lattice_ops.logical_lattice_ops import (
     LogicalLatticeOperation,
+    MultiBodyMeasurement,
     SinglePatchMeasurement,
 )
 import pytest
@@ -28,3 +30,11 @@ class TestSinglePatchMeasurement:
         qubit_id = uuid.uuid4()
         patch_op = SinglePatchMeasurement(qubit_id, m.get_op(0))
         assert patch_op.get_operating_patches() == [qubit_id]
+
+
+class TestMultiBodyMeasurement:
+    def test_get_operating_patches(self):
+        multi_measurement = Measurement.from_list([X, Z, X, Z])
+        patch_operator_map = dict([(uuid.uuid4(), op) for op in multi_measurement.ops_list])
+        multi_body_patch_op = MultiBodyMeasurement(patch_operator_map)
+        assert multi_body_patch_op.get_operating_patches() == list(patch_operator_map.keys())
