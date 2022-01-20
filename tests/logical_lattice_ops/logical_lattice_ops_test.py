1c63f81d6d54c384ad49566235bfc78d0ac2417c
varunseshadri <varunseshadri@gmail.com>
2022-01-20 13:28:38 +0100

    added tests for helper class SinglePatchMeasurement

--- tests/logical_lattice_ops/logical_lattice_ops_test.py
+++ tests/logical_lattice_ops/logical_lattice_ops_test.py
@@ -1,6 +1,18 @@
+from unittest.mock import patch
 import pytest
-from lsqecc.logical_lattice_ops.logical_lattice_ops import LogicalLatticeOperation
+from lsqecc.pauli_rotations import PauliOpCircuit, PauliRotation, Measurement, PauliOperator
+
+from lsqecc.logical_lattice_ops.logical_lattice_ops import (
+    LogicalLatticeOperation,
+    SinglePatchMeasurement,
+)
 import pytest
+import uuid
+
+I = PauliOperator.I
+X = PauliOperator.X
+Y = PauliOperator.Y
+Z = PauliOperator.Z
 
 
 class TestLogicalLatticeOperation:
@@ -8,3 +20,11 @@ class TestLogicalLatticeOperation:
         op = LogicalLatticeOperation()
         with pytest.raises(NotImplementedError):
             op.get_operating_patches()
+
+
+class TestSinglePatchMeasurement:
+    def test_get_operating_patches(self):
+        m = Measurement.from_list([X])
+        qubit_id = uuid.uuid4()
+        patch_op = SinglePatchMeasurement(qubit_id, m.get_op(0))
+        assert patch_op.get_operating_patches() == [qubit_id]
