e0dfd81c361dbd5b24a55f0138d7f1fe32312e46
varunseshadri <varunseshadri@gmail.com>
2022-01-20 22:21:31 +0100

    tests for num_logical_qubits and test_count_magic_states

--- tests/logical_lattice_ops/logical_lattice_ops_test.py
+++ tests/logical_lattice_ops/logical_lattice_ops_test.py
@@ -1,17 +1,22 @@
+from fractions import Fraction
 import uuid
+from black import List
 
 import pytest
+from lsqecc.pauli_rotations.circuit import PauliOpCircuit
+from lsqecc.pauli_rotations.rotation import PauliRotation
 
 import lsqecc.simulation.qubit_state as qs
 from lsqecc.logical_lattice_ops.logical_lattice_ops import (
     AncillaQubitPatchInitialization,
+    LogicalLatticeComputation,
     LogicalLatticeOperation,
     LogicalPauli,
     MagicStateRequest,
     MultiBodyMeasurement,
     SinglePatchMeasurement,
 )
-from lsqecc.pauli_rotations import Measurement, PauliOperator
+from lsqecc.pauli_rotations import Measurement, PauliOperator, segmented_qasm_parser
 
 I = PauliOperator.I  # noqa: E741
 X = PauliOperator.X
@@ -63,3 +68,44 @@ class TestMagicStateRequest:
         qubit_id = uuid.uuid4()
         magic_state = MagicStateRequest(qubit_id)
         assert magic_state.get_operating_patches() == [qubit_id]
+
+
+def generate_tests_num_logical_qubits():
+    c1 = PauliOpCircuit(2)
+    c1.add_pauli_block(PauliRotation.from_list([X, Z], Fraction(1, 4)))
+    c1.add_pauli_block(PauliRotation.from_list([Z, X], Fraction(1, 8)))
+
+    c2 = PauliOpCircuit(4)
+    c2.add_pauli_block(PauliRotation.from_list([X, I, X, Z], Fraction(1, 2)))
+    c2.add_pauli_block(Measurement.from_list([Z, Z, I, Z]))
+
+    c3 = PauliOpCircuit(1)
+    c3.add_pauli_block(PauliRotation.from_list([X], Fraction(-1, 4)))
+
+    return [(c1, 2), (c2, 4), (c3, 1)]
+
+
+def generate_tests_count_magic_states():
+    with open(
+        "/home/varunseshadri/projects/lattice-surgery-compiler/assets/demo_circuits/nontrivial_state.qasm"
+    ) as input_file:
+        c1 = segmented_qasm_parser.parse_str(input_file.read())
+
+    with open(
+        "/home/varunseshadri/projects/lattice-surgery-compiler/assets/demo_circuits/bell_pair.qasm"
+    ) as input_file:
+        c2 = segmented_qasm_parser.parse_str(input_file.read())
+
+    return [(c1, 1), (c2, 0)]
+
+
+class TestLogicalLatticeComputation:
+    @pytest.mark.parametrize("input, expected", generate_tests_num_logical_qubits())
+    def test_num_logical_qubits(self, input, expected):
+        logical_computation = LogicalLatticeComputation(input)
+        assert logical_computation.num_logical_qubits() == expected
+
+    @pytest.mark.parametrize("input, expected", generate_tests_count_magic_states())
+    def test_count_magic_states(self, input, expected):
+        logical_computation = LogicalLatticeComputation(input)
+        assert logical_computation.count_magic_states() == expected
