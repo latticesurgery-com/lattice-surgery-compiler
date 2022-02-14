aa78daa74d1b9d881e8aef958d8ec72bf62f382d
George Watkins <gwwatkin@sfu.ca>
2022-02-14 06:55:30 -0800

    Add debug file

--- debug/debug_manual_qasm_parse.py
+++ debug/debug_manual_qasm_parse.py
@@ -4,15 +4,8 @@ from lsqecc.pauli_rotations.circuit import *
 from app_oriented_benchmarks_adapted import qft_benchmark
 
 
-"""
-
-"""
-
-
-
 if __name__ == "__main__":
     qasm = qft_benchmark.qft_gate(4).qasm()
-    print()
-
+    print(qasm)
     print(PauliOpCircuit._manual_parse_from_reversible_qasm(qasm))
 
