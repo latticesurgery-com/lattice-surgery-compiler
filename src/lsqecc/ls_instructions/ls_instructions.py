d49f8406bbb8b323a78fd8905d72e49c9ca355a2
George Watkins <gwwatkin@sfu.ca>
2022-02-15 10:30:29 -0800

    Generate ls-instructions as text

--- src/lsqecc/ls_instructions/ls_instructions.py
+++ src/lsqecc/ls_instructions/ls_instructions.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Dict
+from typing import Dict, List
 
 import lsqecc.simulation.qubit_state as qs
 from lsqecc.pauli_rotations import PauliOperator
@@ -8,44 +8,88 @@ PatchId = int
 
 
 class LSInstruction:
-    pass
+    def get_args(self) -> List[str]:
+        raise NotImplementedError
 
 
+@dataclass
 class DeclareLogicalQubitPatches(LSInstruction):
-    patch_ids: PatchId = field(default_factory=lambda: [])
+    patch_ids: List[PatchId] = field(default_factory=lambda: [])
+
+    def __repr__(self):
+        return f"{type(self).__name__} {','.join(map(repr,self.patch_ids))}"
+
 
 @dataclass
 class Init(LSInstruction):
     patch_id: PatchId = -1
     state: qs.QubitState = qs.DefaultSymbolicStates.Zero
 
+    def __repr__(self):
+        return f"{type(self).__name__} {self.patch_id} {self.state}"
+
+
 @dataclass
 class RequestMagicState(LSInstruction):
     patch_id: PatchId = -1
+
+    def __repr__(self):
+        return f"{type(self).__name__}  {self.patch_id}"
+
 
 @dataclass
 class RequestYState(LSInstruction):
     patch_id: PatchId = -1
 
+    def __repr__(self):
+        return f"{type(self).__name__}  {self.patch_id}"
+
+
 @dataclass
 class MultiBodyMeasure(LSInstruction):
-    patch_pauli_map : Dict[PatchId,PauliOperator] = field(default_factory=lambda: {})
+    patch_pauli_map: Dict[PatchId, PauliOperator] = field(default_factory=lambda: {})
+
+    def __repr__(self):
+        return (
+            type(self).__name__
+            + " "
+            + ",".join(f"{patch_id}:{op}" for patch_id, op in self.patch_pauli_map.items())
+        )
+
 
 @dataclass
 class MeasureSinglePatch(LSInstruction):
     patch_id: PatchId = -1
-    op : PauliOperator = PauliOperator.Z
+    op: PauliOperator = PauliOperator.Z
+
+    def __repr__(self):
+        return f"{type(self).__name__} {self.patch_id} {self.op}"
+
 
 @dataclass
 class LogicalPauli(LSInstruction):
     patch_id: PatchId = -1
-    op : PauliOperator = PauliOperator.X
+    op: PauliOperator = PauliOperator.X
+
+    def __repr__(self):
+        return f"{type(self).__name__} {self.patch_id} {self.op}"
+
 
 @dataclass
 class SGate(LSInstruction):
     patch_id: PatchId = -1
 
+    def __repr__(self):
+        return f"{type(self).__name__} {self.patch_id}"
+
+
 @dataclass
 class HGate(LSInstruction):
-    patc_id: PatchId = -1
+    patch_id: PatchId = -1
 
+    def __repr__(self):
+        return f"{type(self).__name__} {self.patch_id}"
+
+
+if __name__ == "__main__":
+    print("ok")
--- src/lsqecc/ls_instructions/ls_instructions_from_gates.py
+++ src/lsqecc/ls_instructions/ls_instructions_from_gates.py
@@ -1,12 +1,14 @@
-from typing import List, Sequence
+import itertools
+from typing import Sequence
 
+import lsqecc.simulation.qubit_state as qs
 from lsqecc.gates import gates
+from lsqecc.gates.gates_circuit import GatesCircuit
 from lsqecc.ls_instructions import ls_instructions
 from lsqecc.pauli_rotations import PauliOperator
-import lsqecc.simulation.qubit_state as qs
 
 
-class InstructionGenerator:
+class LSInstructionsFromGatesGenerator:
     def __init__(self):
         self.ancilla_qubit_counter = 0
 
@@ -14,11 +16,11 @@ class InstructionGenerator:
         self.ancilla_qubit_counter += 1
         return self.ancilla_qubit_counter
 
-    def gen_instructions(self, gate : gates.Gate) -> Sequence[ls_instructions.LSInstruction]:
-        if isinstance(gate,gates.X):
-            return [ls_instructions.LogicalPauli(gate.target_qubit,PauliOperator.X)]
-        elif isinstance(gate,gates.Z):
-            return [ls_instructions.LogicalPauli(gate.target_qubit,PauliOperator.Z)]
+    def gen_instructions(self, gate: gates.Gate) -> Sequence[ls_instructions.LSInstruction]:
+        if isinstance(gate, gates.X):
+            return [ls_instructions.LogicalPauli(gate.target_qubit, PauliOperator.X)]
+        elif isinstance(gate, gates.Z):
+            return [ls_instructions.LogicalPauli(gate.target_qubit, PauliOperator.Z)]
         elif isinstance(gate, gates.S):
             return [ls_instructions.SGate(gate.target_qubit)]
         elif isinstance(gate, gates.T):
@@ -26,10 +28,10 @@ class InstructionGenerator:
             return [
                 ls_instructions.RequestMagicState(ancilla),
                 ls_instructions.MultiBodyMeasure(
-                    {gate.target_qubit:PauliOperator.Z, ancilla:PauliOperator.X}
+                    {gate.target_qubit: PauliOperator.Z, ancilla: PauliOperator.X}
                 ),
                 ls_instructions.SGate(gate.target_qubit),
-                ls_instructions.LogicalPauli(gate.target_qubit)
+                ls_instructions.LogicalPauli(gate.target_qubit),
             ]
         elif isinstance(gate, gates.H):
             return [ls_instructions.HGate(gate.target_qubit)]
@@ -43,8 +45,13 @@ class InstructionGenerator:
                 ls_instructions.MultiBodyMeasure(
                     {gate.target_qubit: PauliOperator.X, ancilla: PauliOperator.X}
                 ),
-                ls_instructions.MeasureSinglePatch(gate.control_qubit,PauliOperator.Z)
+                ls_instructions.MeasureSinglePatch(gate.control_qubit, PauliOperator.Z),
             ]
         else:
             raise Exception(f"Gate {gate} is not Clifford+T")
 
+    @staticmethod
+    def text_from_gates_circuit(circuit: GatesCircuit) -> str:
+        generator = LSInstructionsFromGatesGenerator()
+        instructions = itertools.chain.from_iterable(map(generator.gen_instructions, circuit.gates))
+        return "\n".join(map(repr, instructions))
