5dfacc8f810272b193ea80db0ca0fd2c23b3f70c
varunseshadri <varunseshadri@gmail.com>
2022-03-23 09:50:22 +0100

    added tests for gates and gates to circuit

--- tests/gates/gates_test.py
+++ tests/gates/gates_test.py
@@ -3,6 +3,7 @@ from fractions import Fraction
 import pytest
 
 from lsqecc.gates import approximate, gates
+from lsqecc.pauli_rotations.rotation import PauliOperator
 
 
 class TestRZ:
@@ -34,3 +35,55 @@ class TestCRZ:
         decomposition.append(gates.CNOT(control_qubit=0, target_qubit=1))
 
         assert crz_gate.to_clifford_plus_t() == decomposition
+
+
+class TestPauliRotations:
+    @pytest.mark.parametrize(
+        "rot_gate, decomposition",
+        [
+            (
+                gates.PauliRotations(target_qubit=0, phase=Fraction(1, 1), axis=PauliOperator.Z),
+                [gates.Z(target_qubit=0)],
+            ),
+            (
+                gates.PauliRotations(target_qubit=0, phase=Fraction(1, 2), axis=PauliOperator.Z),
+                [gates.S(target_qubit=0)],
+            ),
+            (
+                gates.PauliRotations(target_qubit=0, phase=Fraction(1, 4), axis=PauliOperator.Z),
+                [gates.T(target_qubit=0)],
+            ),
+            (
+                gates.PauliRotations(target_qubit=0, phase=Fraction(1, 16), axis=PauliOperator.Z),
+                approximate.approximate_rz(gates.RZ(target_qubit=0, phase=Fraction(1, 16))),
+            ),
+        ],
+    )
+    def test_to_clifford_plus_t_z_axis(self, rot_gate, decomposition):
+        assert rot_gate.to_clifford_plus_t(compress_rotations=False) == decomposition
+
+    @pytest.mark.parametrize(
+        "rot_gate, decomposition",
+        [
+            (
+                gates.PauliRotations(target_qubit=0, phase=Fraction(1, 1), axis=PauliOperator.X),
+                [gates.X(target_qubit=0)],
+            ),
+            (
+                gates.PauliRotations(target_qubit=0, phase=Fraction(1, 2), axis=PauliOperator.X),
+                [gates.H(target_qubit=0), gates.S(target_qubit=0), gates.H(target_qubit=0)],
+            ),
+            (
+                gates.PauliRotations(target_qubit=0, phase=Fraction(1, 4), axis=PauliOperator.X),
+                [gates.H(target_qubit=0), gates.T(target_qubit=0), gates.H(target_qubit=0)],
+            ),
+            (
+                gates.PauliRotations(target_qubit=0, phase=Fraction(1, 16), axis=PauliOperator.X),
+                [gates.H(target_qubit=0)]
+                + approximate.approximate_rz(gates.RZ(target_qubit=0, phase=Fraction(1, 16)))
+                + [gates.H(target_qubit=0)],
+            ),
+        ],
+    )
+    def test_to_clifford_plus_t_x_axis(self, rot_gate, decomposition):
+        assert rot_gate.to_clifford_plus_t(compress_rotations=False) == decomposition
